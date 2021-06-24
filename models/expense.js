981d23dacd8a534a428f16ab65261833d45e85d4
MarekBartczak <marek.bartczak@gmail.com>
2021-06-24 21:05:19 +0200

    add expense model

---
+++ models/expense.js
@@ -0,0 +1,11 @@
+class Expense {
+  constructor(id, date, title, recipient, cost) {
+    this.id = id;
+    this.date = date;
+    this.title = title;
+    this.recipient = recipient;
+    this.cost = cost;
+  }
+}
+
+export default Expense;
