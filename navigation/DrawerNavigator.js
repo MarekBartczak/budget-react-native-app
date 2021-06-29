6cf75aeb1513ecbcc8a3ce2da6b754412798b687
MarekBartczak <marek.bartczak@gmail.com>
2021-06-29 21:28:55 +0200

    set item Color on drawer

--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -4,6 +4,7 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import BudgetNavigator from "../navigation/BudgetNavigator";
 import FixedExpenseScreen from "../screens/fixedExpense/FixedExpenseScreen";
 import RaportScreen from "../screens/raport/RaportScreen";
+import Colors from "../constants/Colors";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 const Drawer = createDrawerNavigator();
@@ -11,7 +12,15 @@ const Drawer = createDrawerNavigator();
 const DrawerNavigator = (props) => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Navigator
+        initialRouteName="Home"
+        drawerType="back"
+        drawerStyle={{ backgroundColor: Colors.banner }}
+        drawerContentOptions={{
+          activeTintColor: Colors.primary,
+          labelStyle: { color: Colors.primary, fontSize: 15 },
+        }}
+      >
         <Drawer.Screen name="Wydatki" component={BudgetNavigator} />
         <Drawer.Screen name="Wydatki Stałe" component={FixedExpenseScreen} />
         <Drawer.Screen name="Raport" component={RaportScreen} />
