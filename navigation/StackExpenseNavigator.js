bec01d87224ed27ea7e164e94959eac7cf000433
MarekBartczak <marek.bartczak@gmail.com>
2021-07-11 11:25:28 +0200

    filter button

--- navigation/StackExpenseNavigator.js
+++ navigation/StackExpenseNavigator.js
@@ -6,13 +6,51 @@ import CategoryScreen from "../screens/expense/CategoryScreen";
 import FavoritePlaceScreen from "../screens/expense/FavoritePlaceScreen";
 import AddMultipleItemScreen from "../screens/expense/AddMultiItemsSetDateAndPlaceScreen";
 import Colors from "../constants/Colors";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 import { TouchableOpacity } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import React from "react";
 
 const Stack = createStackNavigator();
+
+const filter = (title) => {
+  return {
+    title: title,
+    headerRight: () => (
+      <TouchableOpacity
+        style={{
+          padding: 0,
+          justifyContent: "center",
+          alignItems: "center",
+          marginRight: 20,
+        }}
+        onPress={() => {}}
+      >
+        <MaterialIcons name="filter-list" size={34} color={Colors.primary} />
+      </TouchableOpacity>
+    ),
+  };
+};
+
+const toggleDrawer = (navigation) => {
+  return {
+    headerTitle: "Wydatki",
+    headerLeft: () => (
+      <TouchableOpacity
+        style={{
+          padding: 0,
+          justifyContent: "center",
+          alignItems: "center",
+          marginLeft: 20,
+        }}
+        onPress={() => navigation.toggleDrawer()}
+      >
+        <MaterialCommunityIcons name="menu" size={34} color={Colors.primary} />
+      </TouchableOpacity>
+    ),
+  };
+};
 
 const StackExpenseNavigator = (props) => {
   const stackScreen = (name, component, option) => {
@@ -31,30 +69,15 @@ const StackExpenseNavigator = (props) => {
         },
       }}
     >
-      {stackScreen("Home", MainScreen, ({ navigation }) => ({
-        headerTitle: "Wydatki",
-        headerLeft: () => (
-          <TouchableOpacity
-            style={{
-              padding: 0,
-              justifyContent: "center",
-              alignItems: "center",
-              marginLeft: 20,
-            }}
-            onPress={() => navigation.toggleDrawer()}
-          >
-            <MaterialCommunityIcons
-              name="menu"
-              size={34}
-              color={Colors.primary}
-            />
-          </TouchableOpacity>
-        ),
-      }))}
+      {stackScreen("Home", MainScreen, ({ navigation }) =>
+        toggleDrawer(navigation)
+      )}
       {stackScreen("Details", DetailsScreen, { title: "Szczegóły" })}
-      {stackScreen("Date", DateScreen, { title: "Data" })}
-      {stackScreen("Place", PlaceScreen, { title: "Miejsce" })}
-      {stackScreen("Category", CategoryScreen, { title: "Kategoria" })}
+      {stackScreen("Date", DateScreen, ({ navigation }) => filter("Data"))}
+      {stackScreen("Place", PlaceScreen, ({ navigation }) => filter("Miejsce"))}
+      {stackScreen("Category", CategoryScreen, ({ navigation }) =>
+        filter("Kategoria")
+      )}
 
       {stackScreen("FavoritePlace", FavoritePlaceScreen, {
         title: "Ulubione",
